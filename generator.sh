448084d2454ec3811208804798078743348f691c
Douglas Perkins <webmaster@dperkins.org>
2014-08-21 21:38:23 +0900

    Working on generator.

--- generator.sh
+++ generator.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-# Generates the details files for each elementary school grade, elementary school combined, and junior high school.
+# Generates the details files for each elementary school grade,
+# elementary school combined, and junior high school.
+
+for file in elementary.1.txt elementary.2.txt elementary.3.txt elementary.4.txt elementary.5.txt elementary.6.txt elementary.txt jhs.txt
+do
+	echo "Making details for ${file} ..."
+done
