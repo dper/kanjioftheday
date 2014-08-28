9d088aca0b91347af768ea986b027632dd6c5168
Douglas Perkins <webmaster@dperkins.org>
2014-08-29 00:03:34 +0900

    Adding lists.

--- generator.sh
+++ generator.sh
@@ -3,7 +3,7 @@
 # Generates the details files for each elementary school grade,
 # elementary school combined, and junior high school.
 
-for file in elementary.1.txt elementary.2.txt elementary.3.txt elementary.4.txt elementary.5.txt elementary.6.txt elementary.txt jhs.txt joyo.txt
+for file in elementary.1.txt elementary.2.txt elementary.3.txt elementary.4.txt elementary.5.txt elementary.6.txt elementary.txt jhs.txt joyo.txt jlpt.n5.txt jlpt.n4.txt jlpt.n3.txt jlpt.n2.txt jlpt.n1.txt
 do
 	echo "Making details for ${file} ..."
 	cp lists/${file} targetkanji.txt
