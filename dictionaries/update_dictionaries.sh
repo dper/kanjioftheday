eebf2901a581154d3431617aa6e612574c9323f0
Douglas Perkins <webmaster@dperkins.org>
2015-05-21 23:02:19 +0900

    Adding more dictionaries.

--- dictionaries/update_dictionaries.sh
+++ dictionaries/update_dictionaries.sh
@@ -6,3 +6,9 @@ wget http://ftp.monash.edu.au/pub/nihongo/edict.zip
 unzip edict.zip
 iconv -f EUC-JP -t UTF-8 edict > edict.txt
 rm edict edict.zip edict_doc.html edict.jdx
+
+# Get the word frequency file.
+rm distribution.txt
+wget ftp://ftp.edrdg.org/pub/Nihongo/edict_dupefree_freq_distribution.gz
+gunzip edict_dupefree_freq_distribution.gz
+mv edict_dupefree_freq_distribution distribution.txt
