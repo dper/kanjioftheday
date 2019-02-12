927b9e84703569b4df23b5bd235aef0e5643609b
Douglas Perkins <webmaster@dperkins.org>
2019-02-12 10:23:27 +0900

    Updating downlaod script.

--- dictionaries/update_dictionaries.sh
+++ dictionaries/update_dictionaries.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
 # Get the kanji dictionary.
-rm kanjidic2.xml
-wget http://www.edrdg.org/kanjidic/kanjidic2.xml.gz
+echo "Getting kanji dictionary..."
+wget http://www.edrdg.org/kanjidic/kanjidic2.xml.gz && \
+rm kanjidic2.xml && \
 gunzip kanjidic2.xml.gz
 
 # Get the word dictionary.
-rm edict.txt
-wget http://ftp.monash.edu.au/pub/nihongo/edict.zip
-unzip edict.zip
-iconv -f EUC-JP -t UTF-8 edict > edict.txt
+echo "Getting word dictionary..."
+wget http://ftp.monash.edu.au/pub/nihongo/edict.zip && \
+rm edict.txt && \
+unzip edict.zip && \
+iconv -f EUC-JP -t UTF-8 edict > edict.txt && \
 rm edict edict.zip edict_doc.html edict.jdx
 
 # Get the word frequency file.
-rm distribution.txt
-wget ftp://ftp.edrdg.org/pub/Nihongo/edict_dupefree_freq_distribution.gz
-gunzip edict_dupefree_freq_distribution.gz
+echo "Getting word frequency file..."
+wget ftp://ftp.edrdg.org/pub/Nihongo/edict_dupefree_freq_distribution.gz && \
+rm distribution.txt && \
+gunzip edict_dupefree_freq_distribution.gz && \
 mv edict_dupefree_freq_distribution distribution.txt
